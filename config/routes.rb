f8fa356d630e22373150d8dad6f77d384465fe7d
Tolu Hastrup <adeyt002@gmail.com>
2022-08-29 16:40:45 +0100

    donezo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :dungeons, except: [:create, :edit, :update, :destroy] do
-    resources :bookings, only: :index
+  resources :dungeons, except: [:edit, :update, :destroy] do
+    resources :bookings, except: [:edit, :update, :destroy, :show]
   end
   # Defines the root path route ("/")
   # root "articles#index"
