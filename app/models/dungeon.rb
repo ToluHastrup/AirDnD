196572bc41ae8c8ba2a83b5798d5739e61b1b32b
Michael Barnard <michaeljbarnard100@gmail.com>
2022-08-30 10:54:14 +0100

    done

--- app/models/dungeon.rb
+++ app/models/dungeon.rb
@@ -1,3 +1,10 @@
 class Dungeon < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :guests, presence: true
+  validates :dragon, presence: true
 end
